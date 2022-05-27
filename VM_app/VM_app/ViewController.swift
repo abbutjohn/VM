29a198f56d004f6cff82ed458799c5ba77ab10c2
Abbut <mailme.abbut@gmail.com>
2022-05-27 16:23:31 +0100

    Initial commit

---
+++ VM_app/VM_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VM_app
+//
+//  Created by Anumol Abraham Chakkungal on 27/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
